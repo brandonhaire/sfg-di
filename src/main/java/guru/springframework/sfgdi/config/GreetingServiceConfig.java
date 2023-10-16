61e47b91394a7cfc056d6f3a44cf3972271b7af6
Haire Brandon <Brandon.Haire@ngc.com>
2023-10-16 13:18:38 -0400

    component scan and java configuration example

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorInjectedGreetingService constructorInjectedGreetingService() {
+        return new ConstructorInjectedGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+
+}
