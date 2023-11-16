b5353e05683df6136b3b08b7421277895596c9c6
Haire Brandon <Brandon.Haire@ngc.com>
2023-11-16 11:06:16 -0500

    properties binding

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgdi.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -24,15 +23,11 @@ import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 public class GreetingServiceConfig {
 
     @Bean
-    FakeDataSource fakeDataSource(
-            @Value("${guru.username}") String username, 
-            @Value("${guru.password}") String password,
-            @Value("${guru.jdbcurl}") String jdbcurl
-    ) {
+    FakeDataSource fakeDataSource(SfgConfiguration sfgConfiguration) {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
-        fakeDataSource.setPassword(password);
-        fakeDataSource.setJdbcurl(jdbcurl);
+        fakeDataSource.setUsername(sfgConfiguration.getUsername());
+        fakeDataSource.setPassword(sfgConfiguration.getPassword());
+        fakeDataSource.setJdbcurl(sfgConfiguration.getJdbcurl());
         return fakeDataSource;
     }
 
